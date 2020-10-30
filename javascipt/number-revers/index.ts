354b1dfa8b3308302a53912a11289b4147a3040c
dokyun.kim <dokyun.kim@flitto.com>
2020-10-30 16:51:14 +0900

    feat: space

--- javascipt/number-revers/index.ts
+++ javascipt/number-revers/index.ts
@@ -4,9 +4,9 @@ class FlipInt {
     let result: number = 0
 
     while(n){
-        digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
-        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
-        n = n / 10 | 0  //  Remove right-most digit. Ex. 123 → 12.3 → 12
+      digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
+      result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
+      n = n / 10 | 0  //  Remove right-most digit. Ex. 123 → 12.3 → 12
     }  
 
     return result
